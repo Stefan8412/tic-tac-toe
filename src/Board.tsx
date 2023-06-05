d53a445d6343e9c12d7c04ea10d880d4783224fb
Stefan Hancar <stefan@hancar.sk>
2023-06-05 11:09:34 +0200

    add human move

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,16 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from 'styled-components'
 
+
+const grid=new Array(3**2).fill(null)
+const player_x=1;
+const player_0=2;
 export default function Board() {
-    const grid=new Array(3**2).fill(null)
+const [newGrid,setNewGrid]=useState(grid);
+const [players,setPlayers]=useState({
+    human:player_x,
+    computer:player_0
+})
+const move = (index: number, player: number ) => {
+    setNewGrid((newGrid) => {
+      const gridCopy = newGrid.concat();
+      gridCopy[index] = player;
+    
+      return gridCopy;
+    });
+  };
+
+
+
+const humanMove=(index:number)=>{
+    
+    if(!newGrid[index]){
+        console.log('index', newGrid[index])
+        move(index,players.human)
+      }
+}
   return (
     <Container>
-        {grid.map(()=>{
-
-        
+        {newGrid.map((value,index)=>{
+const isActive=value !== null;
+        console.log(isActive)
         return(
-            <Square>
-{'X'}
+            <Square key={index} onClick={()=>humanMove(index)}>
+{isActive && <Marker>{value===player_x ? "x" : "o"}</Marker> }
             </Square>
         )
         })}
@@ -32,4 +58,7 @@ align-items:center;
 width:100px;
 height:100px;
 border:1px solid green;
+`
+const Marker=styled.p`
+font-size:68px;
 `
