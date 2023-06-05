e56f7361278673c3f46faf71e8095d301c824dd6
Stefan Hancar <stefan@hancar.sk>
2023-06-05 13:05:13 +0200

    computer move

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,10 +1,10 @@
 import React,{useState} from 'react'
 import styled from 'styled-components'
 
-
 const grid=new Array(3**2).fill(null)
 const player_x=1;
 const player_0=2;
+
 export default function Board() {
 const [newGrid,setNewGrid]=useState(grid);
 const [players,setPlayers]=useState({
@@ -15,26 +15,29 @@ const move = (index: number, player: number ) => {
     setNewGrid((newGrid) => {
       const gridCopy = newGrid.concat();
       gridCopy[index] = player;
-    
       return gridCopy;
     });
   };
 
-
+const computerMove=()=>{
+    let index=Math.floor(Math.random()*9)
+    while(newGrid[index]){
+     index=Math.floor(Math.random()*9)
+    }
+    move(index,players.computer)
+}
 
 const humanMove=(index:number)=>{
-    
     if(!newGrid[index]){
-        console.log('index', newGrid[index])
-        move(index,players.human)
+     move(index,players.human)
+     computerMove()
       }
 }
   return (
     <Container>
         {newGrid.map((value,index)=>{
 const isActive=value !== null;
-        console.log(isActive)
-        return(
+       return(
             <Square key={index} onClick={()=>humanMove(index)}>
 {isActive && <Marker>{value===player_x ? "x" : "o"}</Marker> }
             </Square>
